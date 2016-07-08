df0eff54d4db671fc644fc9d85b91df38092dc8d
adyakbar <muh.adyakbar@gmail.com>
2016-07-08 10:39:25 +0700

    membuat method logout

--- app/Http/Controllers/PeriksaController.php
+++ app/Http/Controllers/PeriksaController.php
@@ -25,7 +25,32 @@ class PeriksaController extends Controller
     {
       return view('login');
     }
+  } 
+  /* end of index method */
+
+  public function verifikasi(Request $request)
+  {
+  	$username = $request->username;
+    $password = $request->password;
+
+    // cek apakah dosen / mahasiswa
+    if(preg_match('/^[0-9]{1,}$/', $username))
+    {
+        return $this->dosen($username, $password);
+    }
+    else if(is_numeric(substr($username, 1, strlen($username))))
+    {
+        return $this->mahasiswa($username, $password);
+    }
+  } 
+  /* end of verifikasi method */
+
+  public function logout()
+  {
+      session()->flush();
+      return redirect('/');
   }
+  /* end of logout method */
 }
 
 ?>
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,3 +1,6 @@
 <?php 
 
 Route::get('/', 'PeriksaController@index');
+Route::get('verifikasi', 'PeriksaController@logout');
+Route::get('logout', 'PeriksaController@logout');
+Route::post('verifikasi', 'PeriksaController@verifikasi');
