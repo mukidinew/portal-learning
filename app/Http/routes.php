e282ab805142de3b13bfa897b3e86db82b181c02
adyakbar <muh.adyakbar@gmail.com>
2016-08-11 19:51:37 +0700

    fix kan error email mahasiswa nie

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,6 +5,7 @@ Route::get('verifikasi', 'PeriksaController@logout');
 Route::get('logout', 'PeriksaController@logout');
 Route::post('verifikasi', 'PeriksaController@verifikasi');
 
+Route::get('registrasi-email', 'EmailController@createEmail');
 Route::get('proses-registrasi-email','EmailController@insertEmail');
 Route::get('validasi-email-dosen', 'EmailController@validasiEmailDosen');
 Route::post('validasi-email-dosen', 'EmailController@prosesValidasiEmailDosen');
