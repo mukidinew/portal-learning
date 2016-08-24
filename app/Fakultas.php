d49cdf390799523e9c433b29a4ecdee8e39c9a32
adyakbar <muh.adyakbar@gmail.com>
2016-08-24 11:20:57 +0700

    menambahkan model class Fakultas dan method get_fakultas untuk di jalankan di console getService:fakultas

---
+++ app/Fakultas.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Fakultas extends Model
+{
+    protected $table = 'fakultas';
+
+    public function get_fakultas()
+    {
+    	$last_periode = \App\Periode::max('id');
+    	
+    	$ch = curl_init();
+    	curl_setopt($ch, CURLOPT_URL, 'http://203.24.50.30:9099/datasnap/rest/tservermethods1/fakultas');
+    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    	$service = curl_exec($ch);
+
+    	if($service === false)
+    	{
+    		return "Service Periode Sedang Gangguan";
+    	}
+    	else
+    	{
+    		$data = json_decode($service, TRUE);
+    		foreach($data['result'][0] as $key => $value)
+    		{
+    			$hitung_fakultas = $this->where('fakultas_id', $value['idbagian'])
+    									->where('periode_id', $last_periode)->count();
+    			if($hitung_fakultas == 0)
+    			{
+    				$this->insert(array(
+    					'periode_id' => $last_periode,
+    					'fakultas_id' => $value['idbagian'],
+    					'fakultas' => $value['fakultas']
+    				));
+    			}
+    		}
+    		return "Fakultas periode ".$last_periode." sudah di update";
+    	}
+    }
+}
